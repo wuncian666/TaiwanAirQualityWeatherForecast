f775034d01b28ebe4871d9c193448a6c313d5e30
wuncian <wuncian666@gmail.com>
2023-02-08 10:22:57 +0800

    modify the content as described in the documentation(20230207 code review)

---
+++ app/src/main/java/com/example/airqualityindex/features/user/viewmodel/UserViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.airqualityindex.features.user.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.airqualityindex.shared.repositories.implement.UserDataRepositoryImpl
+
+class UserViewModel(private val repository: UserDataRepositoryImpl) : ViewModel() {
+
+}
---
+++ app/src/main/java/com/example/airqualityindex/shared/constant/MainConfig.kt
@@ -0,0 +1,4 @@
+package com.example.airqualityindex.shared.constant
+
+object MainConfig {
+}
